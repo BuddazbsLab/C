65ca4b673277964eb3a03ea44b201f785579b696
иван <52652515+nomadroom@users.noreply.github.com>
2021-04-06 10:45:26 +0300

    одмен
    На что садится Одмен

---
+++ Example_2/Example_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;
+            string myString;
+            k = 17;
+            myString = "\"Одмен\" сел ровно на булок ";
+            Console.WriteLine("{0} {1}.", myString, k);
+            Console.ReadKey();
+        }
+    }
+}
