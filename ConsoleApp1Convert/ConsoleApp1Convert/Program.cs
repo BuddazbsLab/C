d4fb3f751d55897e6ec1c7948977f7740df06ef7
иван <52652515+nomadroom@users.noreply.github.com>
2021-04-06 10:42:51 +0300

    Преобразование Типов
    Наглядное пособие по преобразованию типов

---
+++ ConsoleApp1Convert/ConsoleApp1Convert/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1Convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short shortRes, shortVal = 4;
+            int intVal = 67;
+            long longRes;
+            float floatVal = 10.5F;
+            double doubleRes, doubleVal = 99.999;
+            string strRes, strVal = "17";
+            bool boolVal = true;
+            Console.WriteLine(" Примеры преобразования ");
+            Console.WriteLine();
+            // Примеры преобразованич переменных
+            doubleRes = floatVal * shortVal;
+            Console.WriteLine("Неявные (double) : {0}*{1} -> {2}",
+                floatVal, shortVal, doubleRes);
+            shortRes = (short)floatVal;
+            Console.WriteLine("Явные (short) : {0} -> {1}",
+                floatVal, shortRes);
+            strRes = Convert.ToString(boolVal) +
+                Convert.ToString(doubleVal);
+            Console.WriteLine("Явные (string): \"{0} \"+\"{1}\" -> {2}", boolVal, doubleVal, strRes);
+            longRes = intVal + Convert.ToInt64(strVal);
+            Console.WriteLine("Смешанные (long) : {0} + {1} -> {2}", intVal, strVal, longRes);
+            Console.ReadKey();
+        }
+    }
+}
