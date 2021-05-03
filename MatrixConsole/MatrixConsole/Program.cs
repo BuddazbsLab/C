db6bf2b1f057be8fe22c0420d974d603f12de654
Budda <buddazbs@yandex.ru>
2021-05-03 21:47:24 +0300

    С

---
+++ MatrixConsole/MatrixConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MatrixConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Wake up, Neo...");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("The Matrix has you..");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Follow the white rabbit.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Knock, knock, Neo.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
