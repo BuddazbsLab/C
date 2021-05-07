e103635eff0df94748f7061f9f65abf7ff4d0a54
иван <52652515+nomadroom@users.noreply.github.com>
2021-05-07 22:23:51 +0300

    Перечисления (enum)
    Перечисления (enum)

---
+++ EnumDays/EnumDays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnumDays
+{
+    class Program
+    {
+        enum Days
+        { 
+            Mon,
+            Tru,
+            Wed,
+            Tro,
+            Fri,
+            Sut,
+            Sun
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Fri);
+            Console.ReadLine();
+
+        }
+    }
+}
