60c882e31378ceeb84121f85db855775055d4af9
иван <52652515+nomadroom@users.noreply.github.com>
2021-05-07 22:11:56 +0300

    Тренировка языка С#
    Домашние уроки по С#

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            array[0] = 1;
+            array[1] = 2;
+            array[2] = 3;
+            array[3] = 4;
+            array[4] = 5;
+            array[5] = 6;
+            array[6] = 7;
+
+            List<int> list = new List<int>();
+            list.Add(0);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            Console.WriteLine(array[3]);
+            Console.WriteLine(list[2]);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
