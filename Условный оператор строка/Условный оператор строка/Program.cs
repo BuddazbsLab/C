a86b6f9c69b354fdd0bd85bd67d08b61b219574b
иван <52652515+nomadroom@users.noreply.github.com>
2021-05-09 17:06:56 +0300

    Условный оператор
    Условный оператор и сравнения

---
+++ Условный оператор строка/Условный оператор строка/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Условный_оператор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            if (input < 10 && input > 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
