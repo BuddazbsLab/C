fde5d5b3180abb980c326c53cf5e0e672a18486f
иван <52652515+nomadroom@users.noreply.github.com>
2021-05-09 18:19:29 +0300

    Свич
    Условный оператор СВИЧ

---
+++ Условный оператор свич/Условный оператор свич/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Условный_оператор_свич
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            //var s = input == 10 ? "да" : "нет";
+            Console.WriteLine(input == 10 ? "да" : "нет");
+            switch (input)
+            {
+                case 1:
+                    //действия 1
+                   break; // оператор выхода
+                case 2:
+                   break;
+                case 3:
+                   break;
+                default:
+                    break;
+            }
+        }
+    }
+}
