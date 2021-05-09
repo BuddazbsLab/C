a880959df084f51c693dd54e9f63dee0d0bcf481
иван <52652515+nomadroom@users.noreply.github.com>
2021-05-09 13:11:02 +0300

    Логический оператор If else
    Знакомство с логическим оператором if

---
+++ IfSwitch/IfSwitch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IfSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = 3 < 5;  //true; // false
+
+            if (3 > 5)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
