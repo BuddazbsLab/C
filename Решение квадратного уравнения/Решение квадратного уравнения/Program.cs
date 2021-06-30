ca582adc8bc8bb63e9e11f0fed2e73a108f7ac28
иван <buddazbs@yandex.ru>
2021-06-30 18:33:51 +0300

    Решение квадратного уравнения

---
+++ Решение квадратного уравнения/Решение квадратного уравнения/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Решение_квадратного_уравнения
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            double a, b, c;
+            Console.Write("a = ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c = ");
+            c = Convert.ToDouble(Console.ReadLine());
+            if (a == 0)
+                if (b == 0)
+                    if (c == 0)
+                        Console.WriteLine("x - любое");
+                    else
+                        Console.WriteLine("Нет корней");
+                else
+                    Console.WriteLine("x = {0}", -c / b);
+            else
+            {
+                double d = b * b - 4 * a * c;
+                if (d > 0)
+                {
+                    d = Math.Sqrt(d);
+                    Console.WriteLine("x1 = {0}", (-b + d) / (2 * a));
+                    Console.WriteLine("x1 = {0}", (-b - d) / (2 * a));
+                }
+                else
+                    Console.WriteLine("Нет вещественных корней");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+            
