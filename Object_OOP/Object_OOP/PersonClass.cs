62879a320de1ac54dee84385645583214f4353fd
иван <buddazbs@yandex.ru>
2021-08-06 14:42:42 +0300

    ПОмойка

---
+++ Object_OOP/Object_OOP/PersonClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Object_OOP
+{
+    class PersonClass
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Михаил", "Фленов"); 
+            Console.WriteLine(p.ToString());  // ToString - превращает класс в строку
+
+            Object o = p;
+            Console.WriteLine(o.ToString());
+            // Equals - сравнение свойств переменных тип bool
+            Person p1 = new Person("Михаил", "Фленов");
+            Person p2 = new Person("Михаил", "Фленов");
+            // p1 и p2 имеют одинаковые значения свойств
+            Person p3 = p1;
+            Console.WriteLine(p1.Equals(p2)); // False/  После переопределения Equals возращает True
+            Console.WriteLine(p1.Equals(p3)); // True 
+        }
+    }
+}
