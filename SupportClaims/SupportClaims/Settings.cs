d209b5f179130a62032fc49439d479afa6f95c98
иван <buddazbs@yandex.ru>
2021-11-01 12:13:07 +0300

    Deserialize SettingsApp

---
+++ SupportClaims/SupportClaims/Settings.cs
@@ -0,0 +1,12 @@
+﻿namespace SupportClaims
+{
+    public class Settings
+    {
+        public class AppData
+        {
+            public string token { get; set; }
+            public string basgeteurl { get; set; }
+            public string stiker { get; set; }
+        }
+    }
+}
